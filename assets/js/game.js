3169c0749ad497b1f59dbcce028618cb3013f41d
Jaswal1p <Jaswal1p@yahoo.com>
2023-01-04 08:08:01 -0500

    math with operators

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,12 +17,26 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
-    window.alert("WELCOME to Robot Gladiators!")
+    // Alert players of game start
+    window.alert("WELCOME to Robot Gladiators!");
+
+    // subtract value of 'playerAttack' from 'enemyHealth' & update new value of 'enemyHealth'. 
+    enemyHealth = enemyHealth - playerAttack;
+
+    // log a resulting message to the console
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // subtract value of 'enemyAttack' from 'playerHealth' & update new value of 'playerHealth'. 
+    playerHealth = playerHealth - enemyAttack;
+
+    // log a resulting message to the console
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining!");
+
 };
 
-var endFight = function () {
-    window.alert("Fight has ended")
-};
+// var endFight = function () {
+//     window.alert("Fight has ended")
+// };
 
 // creating a function named "fight"
 
