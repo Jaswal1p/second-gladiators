8b556d4bbc28f2992b502a7eb21b1eed481f977e
Jaswal1p <Jaswal1p@yahoo.com>
2023-01-04 07:08:00 -0500

    tried console with data types & concatenation

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,19 @@
 
 var playerName = window.prompt("what is your robot's name");
 
-window.alert(playerName);
+// what is this?
+
+console.log(playerName);
+
+console.log("This logs creation of new medical software");
+
+console.log(10 + 100);
+
+console.log("10" + "/" + "13" + "/" + "2002");
+
+console.log("Our robot's name is " + playerName);
+
+// window.alert(playerName);
 
 // creating a function named "fight"
 
