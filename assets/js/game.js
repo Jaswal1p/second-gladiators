9715479319e35540b546e677d448914a347940d1
Jaswal1p <Jaswal1p@yahoo.com>
2023-01-04 06:48:52 -0500

    learning alert function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Javascript is running!");
+// creating  prompt
+
+var playerName = window.prompt("what is your robot's name");
+
+window.alert(playerName);
+
+// creating a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun");
+
+}
+
+//fight();
